af7648730fb6242f04e589b224563ded8bf14b2f
Jack <jackroth49@gmail.com>
2025-03-20 16:23:43 -0400

    updating noaa api to include original api, more useful, added methods to interact with it to retrieve info, alerts, map location data, etc

--- data/main.py
+++ data/main.py
@@ -1,33 +1,55 @@
 from NOAA_API import NOAA_API_Client
+from FEMA_API import FEMA_API_Client
 
 def main():
     noaa = NOAA_API_Client()
 
-    locations = noaa.get_endpoint(endpoint='locations')
+    #get api info
+    noaa.get_api_info()
+
+    #get endpoints
+    noaa.get_api_endpoints()
+
+    #getting all state zone ids
+    ca_zones = noaa.get_state_zone_ids(state='CA')
+    #print(ca_zones)
+
+    #checking alerts for individual zones | CAC036 - no alerts | CAC037 - has alerts
+    alerts = noaa.get_alerts_for_zone('CAC036')
+    print(alerts)
+    alerts = noaa.get_alerts_for_zone('CAC037')
+    print(alerts)
+
+    #getting geometry for zones
+    geometry_data = noaa.get_endpoint('zones/county/CAC037')
+    g = geometry_data['geometry']
+    print(g) #cordinates for graphs
+    
+    #noaa api v2 examples
+    locations = noaa.get_endpoint_v2(endpoint='locations')
     locations_metadata = locations['metadata']
     locations_list = locations['results']
     first_location = locations_list[0]
     location_id = first_location['id']
     print(first_location)
 
-    datasets = noaa.get_endpoint(endpoint='datasets', params={'limit': 10, 'offset': 0, 'locationid': location_id})
+    datasets = noaa.get_endpoint_v2(endpoint='datasets', params={'limit': 10, 'offset': 0, 'locationid': location_id})
     datasets_metadata = datasets['metadata']
     datasets_list = datasets['results']
     first_dataset = datasets_list[0]
     dataset_id = first_dataset['id']
     print(first_dataset)
 
-    data = noaa.get_endpoint(endpoint='data', params={'startdate': '2025-01-01', 'enddate': '2025-01-02', 'datasetid': 'GSOM'})
+    data = noaa.get_endpoint_v2(endpoint='data', params={'startdate': '2025-01-01', 'enddate': '2025-01-02', 'datasetid': 'GSOM'})
     data_metadata = data['metadata']
     data_list = data['results']
     first_data = data_list[0]
     print(first_data)
     
-    datatypes = noaa.get_endpoint(endpoint='datatypes', params={'datasetid': 'GSOM'})
+    datatypes = noaa.get_endpoint_v2(endpoint='datatypes', params={'datasetid': 'GSOM'})
     datatypes_metadata = datatypes['metadata']
     datatypes_list = datatypes['results']
     first_datatype = datatypes_list[0]
     print(first_datatype)
-
 
 main()
