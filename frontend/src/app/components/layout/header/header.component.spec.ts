8f7b3ad2ed602f37b84a80bbf76893b7966bf485
BJ Burns <burnsbj23@gmail.com>
2025-04-16 14:44:13 -0500

    fix header tests

--- frontend/src/app/components/layout/header/header.component.spec.ts
+++ frontend/src/app/components/layout/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { MenuComponent } from '../menu/menu.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +9,10 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
+      declarations: [
+        HeaderComponent,
+        MenuComponent
+        ]
     })
     .compileComponents();
 
