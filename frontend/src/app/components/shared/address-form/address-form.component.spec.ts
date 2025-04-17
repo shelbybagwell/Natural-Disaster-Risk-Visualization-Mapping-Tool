65d2ee3ddbde3975fd3584bab99a891a54884b88
BJ Burns <burnsbj23@gmail.com>
2025-04-17 04:08:36 -0500

    add mat modules to address form tests

--- frontend/src/app/components/shared/address-form/address-form.component.spec.ts
+++ frontend/src/app/components/shared/address-form/address-form.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddressFormComponent } from './address-form.component';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 describe('AddressFormComponent', () => {
   let component: AddressFormComponent;
@@ -8,6 +12,12 @@ describe('AddressFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        ReactiveFormsModule,
+      ],
       declarations: [AddressFormComponent]
     })
     .compileComponents();
